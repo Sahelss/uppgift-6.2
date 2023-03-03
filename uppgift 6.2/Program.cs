bfab1a2e9f15edfc5f4d7b70ad59286a3f246bd5
EEDAT <EEDAT@TEB114105>
2023-03-03 14:39:16 +0100

    Add project files.

---
+++ uppgift 6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int tal1 = 212;
+        int tal2 = 111;
+        int störstaTal = Störst(tal1, tal2);
+        Console.WriteLine("Det största talet är " + störstaTal);
+    }
+
+    static int Störst(int tal1, int tal2)
+    {
+        if (tal1 == tal2)
+        {
+            return tal1;
+        }
+        else if (tal1 > tal2)
+        {
+            return tal1;
+        }
+        else
+        {
+            return tal2;
+        }
+    }
+}
